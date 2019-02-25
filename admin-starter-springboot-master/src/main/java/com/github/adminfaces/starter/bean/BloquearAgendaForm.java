64994797d85b95877dfb49c85e719cac3cf87296
Ruanoid <35999705+Ruanoid@users.noreply.github.com>
2019-02-25 11:45:12 -0300

    att

--- admin-starter-springboot-master/src/main/java/com/github/adminfaces/starter/bean/BloquearAgendaForm.java
+++ admin-starter-springboot-master/src/main/java/com/github/adminfaces/starter/bean/BloquearAgendaForm.java
@@ -3,7 +3,11 @@ package com.github.adminfaces.starter.bean;
 import com.github.adminfaces.starter.model.HorarioAgendado;
 import com.github.adminfaces.starter.repository.HorarioAgendadoRepository;
 
-import org.primefaces.component.calendar.Calendar;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -12,17 +16,39 @@ import org.springframework.stereotype.Component;
 @Scope("view")
 public class BloquearAgendaForm extends AbastractFormBean<HorarioAgendado, HorarioAgendadoRepository> {
 
-	private Calendar inicio,termino = new Calendar();
+	private Date inicio,termino ;
+	private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 	
 	public BloquearAgendaForm() {
 		super(HorarioAgendado.class);
 	}
 	@Override
-	public void salvar() {
-		//getObjeto().setData(inicio.getTime());
-		System.out.println(inicio);
+	public void salvar() throws InstantiationException, IllegalAccessException {
+		System.out.println("--------");
+		System.out.println(simpleDateFormat.format(inicio));
+		Time hora =  inicio.get;
+		System.out.println(simpleDateFormat.format(inicio));
+	
 		System.out.println(termino);
+		System.out.println("--------");
 	}
+	public Date getInicio() {
+		return inicio;
+	}
+	public void setInicio(Date inicio) {
+		this.inicio = inicio;
+	}
+	public Date getTermino() {
+		return termino;
+	}
+	public void setTermino(Date termino) {
+		this.termino = termino;
+	}
+	
+	
+	
+	
+	
 	
    
 }

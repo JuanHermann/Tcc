80c24d6cd985b94b27e3b29ce58a1a26e925898d
Ruanoid <35999705+Ruanoid@users.noreply.github.com>
2019-02-25 16:50:20 -0300

    att

--- admin-starter-springboot-master/src/main/java/com/github/adminfaces/starter/bean/BloquearAgendaForm.java
+++ admin-starter-springboot-master/src/main/java/com/github/adminfaces/starter/bean/BloquearAgendaForm.java
@@ -1,9 +1,11 @@
 package com.github.adminfaces.starter.bean;
 
+import com.github.adminfaces.starter.converter.TimeConverter;
 import com.github.adminfaces.starter.model.HorarioAgendado;
 import com.github.adminfaces.starter.repository.HorarioAgendadoRepository;
 
 import java.sql.Time;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -16,8 +18,9 @@ import org.springframework.stereotype.Component;
 @Scope("view")
 public class BloquearAgendaForm extends AbastractFormBean<HorarioAgendado, HorarioAgendadoRepository> {
 
-	private Date inicio,termino ;
-	private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+	private String inicio,termino ;
+	private SimpleDateFormat data = new SimpleDateFormat("dd/MM/yyyy");
+	private SimpleDateFormat hora = new SimpleDateFormat("HH:mm:ss");
 	
 	public BloquearAgendaForm() {
 		super(HorarioAgendado.class);
@@ -25,25 +28,32 @@ public class BloquearAgendaForm extends AbastractFormBean<HorarioAgendado, Horar
 	@Override
 	public void salvar() throws InstantiationException, IllegalAccessException {
 		System.out.println("--------");
-		System.out.println(simpleDateFormat.format(inicio));
-		Time hora =  inicio.get;
-		System.out.println(simpleDateFormat.format(inicio));
+		System.out.println(data.format(inicio));
+		Date data = new Date();
+		try {
+			data = hora.parse(inicio);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Time time = new Time(data.getTime());
 	
 		System.out.println(termino);
 		System.out.println("--------");
 	}
-	public Date getInicio() {
+	public String getInicio() {
 		return inicio;
 	}
-	public void setInicio(Date inicio) {
+	public void setInicio(String inicio) {
 		this.inicio = inicio;
 	}
-	public Date getTermino() {
+	public String getTermino() {
 		return termino;
 	}
-	public void setTermino(Date termino) {
+	public void setTermino(String termino) {
 		this.termino = termino;
 	}
+
 	
 	
 	

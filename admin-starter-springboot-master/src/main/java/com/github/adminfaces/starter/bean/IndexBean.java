12513e1e4850de36f554b5803238ebcd7ddb7c24
Ruanoid <35999705+Ruanoid@users.noreply.github.com>
2019-03-01 16:55:45 -0300

    att

--- admin-starter-springboot-master/src/main/java/com/github/adminfaces/starter/bean/IndexBean.java
+++ admin-starter-springboot-master/src/main/java/com/github/adminfaces/starter/bean/IndexBean.java
@@ -1,9 +1,12 @@
 package com.github.adminfaces.starter.bean;
 
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -25,88 +28,107 @@ import org.springframework.stereotype.Component;
 import com.github.adminfaces.starter.model.HorarioAgendado;
 import com.github.adminfaces.starter.model.Servico;
 import com.github.adminfaces.starter.model.Usuario;
+import com.github.adminfaces.starter.model.UsuarioServico;
 import com.github.adminfaces.starter.repository.HorarioAgendadoRepository;
 import com.github.adminfaces.starter.repository.ServicoRepository;
 import com.github.adminfaces.starter.repository.UsuarioRepository;
+import com.github.adminfaces.starter.repository.UsuarioServicoRepository;
 
 @Component
 @Scope("view")
 public class IndexBean extends AbastractFormBean<HorarioAgendado, HorarioAgendadoRepository> {
-	
+
 	private ScheduleModel eventModel = new DefaultScheduleModel();
-	
+
 	private ScheduleModel lazyEventModel;
-	
-	 private ScheduleEvent event = new DefaultScheduleEvent();
-	
-	 private String tipo ="servico";
-	 private Time horarios;
-	 
-	 @Autowired
-	private UsuarioRepository usuarioRepository;		
-	private List<Usuario> funcionarios;
-	
+
+	private ScheduleEvent event = new DefaultScheduleEvent();
+
+	private String tipo = "servico";
+	private Time horarios;
+
 	@Autowired
-	private ServicoRepository servicoRepository;		
-	private List<Servico> servicos;
+	private UsuarioRepository usuarioRepository;
+	private List<Usuario> funcionarios;
+	private Set<Usuario> setFuncionarios;
 	
+
+	@Autowired
+	private ServicoRepository servicoRepository;
+	private List<Servico> servicos;
+
 	private List<Servico> servicosSelecionados;
+
+	@Autowired
+	private UsuarioServicoRepository usuarioServicoRepository;
+	private List<UsuarioServico> usuarioServicos;
 
 	public IndexBean() {
 		super(HorarioAgendado.class);
 
 	}
-	
+
 	@PostConstruct
-    public void init() {
-		funcionarios = usuarioRepository.findAll();
+	public void init() {
 		servicos = servicoRepository.findAll();
-		
-        eventModel = new DefaultScheduleModel();
-        eventModel.addEvent(new DefaultScheduleEvent("Champions League Match", previousDay8Pm(), previousDay11Pm()));
-        eventModel.addEvent(new DefaultScheduleEvent("Birthday Party", today1Pm(), today6Pm()));
-        eventModel.addEvent(new DefaultScheduleEvent("Breakfast at Tiffanys", nextDay9Am(), nextDay11Am()));
-        eventModel.addEvent(new DefaultScheduleEvent("Plant the new garden stuff", theDayAfter3Pm(), fourDaysLater3pm()));
-        
-         
-        lazyEventModel = new LazyScheduleModel() {
-             
-            
-            public void loadEvents(Date start, Date end) {
-                Date random = getRandomDate(start);
-                addEvent(new DefaultScheduleEvent("Lazy Event 1", random, random));
-                 
-                random = getRandomDate(start);
-                addEvent(new DefaultScheduleEvent("Lazy Event 2", random, random));
-            }   
-        };
-    }
-	
-	public boolean mostrarForm() {
-		System.out.println(tipo);
-		System.out.println("servico".equalsIgnoreCase(tipo));
-		return "servico".equalsIgnoreCase(tipo);
-		
+		funcionarios = new ArrayList<>();
+		setFuncionarios = new HashSet<>();
+		eventModel = new DefaultScheduleModel();
+		eventModel.addEvent(new DefaultScheduleEvent("Champions League Match", previousDay8Pm(), previousDay11Pm()));
+		eventModel.addEvent(new DefaultScheduleEvent("Birthday Party", today1Pm(), today6Pm()));
+		eventModel.addEvent(new DefaultScheduleEvent("Breakfast at Tiffanys", nextDay9Am(), nextDay11Am()));
+		eventModel.addEvent(new DefaultScheduleEvent("Plant the new garden stuff", theDayAfter3Pm(), fourDaysLater3pm()));
+
+		lazyEventModel = new LazyScheduleModel() {
+
+			public void loadEvents(Date start, Date end) {
+				Date random = getRandomDate(start);
+				addEvent(new DefaultScheduleEvent("Lazy Event 1", random, random));
+
+				random = getRandomDate(start);
+				addEvent(new DefaultScheduleEvent("Lazy Event 2", random, random));
+			}
+		};
 	}
-	
+
+	public void buscarFuncionarios() {
+		System.out.println("passou");
+		if (servicosSelecionados != null) {
+			for (Servico servico : servicosSelecionados) {
+				usuarioServicos = usuarioServicoRepository.findByServicoOrderByUsuario(servico);
+				for (UsuarioServico usuarioServico : usuarioServicos) {
+					setFuncionarios.add(usuarioServico.getUsuario());
+//					System.out.println("top");
+				}
+			}
+		}
+	}
+
+	public boolean mostrarForm() {
+//		System.out.println(tipo);
+//		System.out.println("servico".equalsIgnoreCase(tipo));
+		return "servico".equalsIgnoreCase(tipo);
+
+	}
+
 	public void salvarAgendamento() {
-		
+
 	}
 
 	public Date getRandomDate(Date base) {
-        Calendar date = Calendar.getInstance();
-        date.setTime(base);
-        date.add(Calendar.DATE, ((int) (Math.random()*30)) + 1);    //set random day of month
-         
-        return date.getTime();
-    }
-	
+		Calendar date = Calendar.getInstance();
+		date.setTime(base);
+		date.add(Calendar.DATE, ((int) (Math.random() * 30)) + 1); // set random day of month
+
+		return date.getTime();
+	}
+
 	public Date getInitialDate() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(calendar.get(Calendar.YEAR), Calendar.FEBRUARY, calendar.get(Calendar.DATE), 0, 0, 0);
-         
-        return calendar.getTime();
-    }
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(calendar.get(Calendar.YEAR), Calendar.FEBRUARY, calendar.get(Calendar.DATE), 0, 0, 0);
+
+		return calendar.getTime();
+	}
 
 	public ScheduleModel getEventModel() {
 		return eventModel;
@@ -115,125 +137,128 @@ public class IndexBean extends AbastractFormBean<HorarioAgendado, HorarioAgendad
 	public void setEventModel(ScheduleModel eventModel) {
 		this.eventModel = eventModel;
 	}
+
 	private Calendar today() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE), 0, 0, 0);
- 
-        return calendar;
-    }
-     
-    private Date previousDay8Pm() {
-        Calendar t = (Calendar) today().clone();
-        t.set(Calendar.AM_PM, Calendar.PM);
-        t.set(Calendar.DATE, t.get(Calendar.DATE) - 1);
-        t.set(Calendar.HOUR, 8);
-         
-        return t.getTime();
-    }
-     
-    private Date previousDay11Pm() {
-        Calendar t = (Calendar) today().clone();
-        t.set(Calendar.AM_PM, Calendar.PM);
-        t.set(Calendar.DATE, t.get(Calendar.DATE) - 1);
-        t.set(Calendar.HOUR, 11);
-         
-        return t.getTime();
-    }
-     
-    private Date today1Pm() {
-        Calendar t = (Calendar) today().clone();
-        t.set(Calendar.AM_PM, Calendar.PM);
-        t.set(Calendar.HOUR, 1);
-         
-        return t.getTime();
-    }
-     
-    private Date theDayAfter3Pm() {
-        Calendar t = (Calendar) today().clone();
-        t.set(Calendar.DATE, t.get(Calendar.DATE) + 2);     
-        t.set(Calendar.AM_PM, Calendar.PM);
-        t.set(Calendar.HOUR, 3);
-         
-        return t.getTime();
-    }
- 
-    private Date today6Pm() {
-        Calendar t = (Calendar) today().clone(); 
-        t.set(Calendar.AM_PM, Calendar.PM);
-        t.set(Calendar.HOUR, 6);
-         
-        return t.getTime();
-    }
-     
-    private Date nextDay9Am() {
-        Calendar t = (Calendar) today().clone();
-        t.set(Calendar.AM_PM, Calendar.AM);
-        t.set(Calendar.DATE, t.get(Calendar.DATE) + 1);
-        t.set(Calendar.HOUR, 9);
-         
-        return t.getTime();
-    }
-     
-    private Date nextDay11Am() {
-        Calendar t = (Calendar) today().clone();
-        t.set(Calendar.AM_PM, Calendar.AM);
-        t.set(Calendar.DATE, t.get(Calendar.DATE) + 1);
-        t.set(Calendar.HOUR, 11);
-         
-        return t.getTime();
-    }
-     
-    private Date fourDaysLater3pm() {
-        Calendar t = (Calendar) today().clone(); 
-        t.set(Calendar.AM_PM, Calendar.PM);
-        t.set(Calendar.DATE, t.get(Calendar.DATE) + 4);
-        t.set(Calendar.HOUR, 3);
-         
-        return t.getTime();
-    }
-     
-    public ScheduleEvent getEvent() {
-        return event;
-    }
- 
-    public void setEvent(ScheduleEvent event) {
-        this.event = event;
-    }
-     
-    public void addEvent() {
-        if(event.getId() == null)
-            eventModel.addEvent(event);
-        else
-            eventModel.updateEvent(event);
-         
-        event = new DefaultScheduleEvent();
-    }
-     
-    public void onEventSelect(SelectEvent selectEvent) {
-        event = (ScheduleEvent) selectEvent.getObject();
-    }
-     
-    public void onDateSelect(SelectEvent selectEvent) {
-        event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
-    }
-     
-    public void onEventMove(ScheduleEntryMoveEvent event) {
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
-         
-        addMessage(message);
-    }
-     
-    public void onEventResize(ScheduleEntryResizeEvent event) {
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
-         
-        addMessage(message);
-    }
-     
-    private void addMessage(FacesMessage message) {
-        FacesContext.getCurrentInstance().addMessage(null, message);
-    }
-    
-    public List<Usuario> getFuncionarios() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE), 0, 0, 0);
+
+		return calendar;
+	}
+
+	private Date previousDay8Pm() {
+		Calendar t = (Calendar) today().clone();
+		t.set(Calendar.AM_PM, Calendar.PM);
+		t.set(Calendar.DATE, t.get(Calendar.DATE) - 1);
+		t.set(Calendar.HOUR, 8);
+
+		return t.getTime();
+	}
+
+	private Date previousDay11Pm() {
+		Calendar t = (Calendar) today().clone();
+		t.set(Calendar.AM_PM, Calendar.PM);
+		t.set(Calendar.DATE, t.get(Calendar.DATE) - 1);
+		t.set(Calendar.HOUR, 11);
+
+		return t.getTime();
+	}
+
+	private Date today1Pm() {
+		Calendar t = (Calendar) today().clone();
+		t.set(Calendar.AM_PM, Calendar.PM);
+		t.set(Calendar.HOUR, 1);
+
+		return t.getTime();
+	}
+
+	private Date theDayAfter3Pm() {
+		Calendar t = (Calendar) today().clone();
+		t.set(Calendar.DATE, t.get(Calendar.DATE) + 2);
+		t.set(Calendar.AM_PM, Calendar.PM);
+		t.set(Calendar.HOUR, 3);
+
+		return t.getTime();
+	}
+
+	private Date today6Pm() {
+		Calendar t = (Calendar) today().clone();
+		t.set(Calendar.AM_PM, Calendar.PM);
+		t.set(Calendar.HOUR, 6);
+
+		return t.getTime();
+	}
+
+	private Date nextDay9Am() {
+		Calendar t = (Calendar) today().clone();
+		t.set(Calendar.AM_PM, Calendar.AM);
+		t.set(Calendar.DATE, t.get(Calendar.DATE) + 1);
+		t.set(Calendar.HOUR, 9);
+
+		return t.getTime();
+	}
+
+	private Date nextDay11Am() {
+		Calendar t = (Calendar) today().clone();
+		t.set(Calendar.AM_PM, Calendar.AM);
+		t.set(Calendar.DATE, t.get(Calendar.DATE) + 1);
+		t.set(Calendar.HOUR, 11);
+
+		return t.getTime();
+	}
+
+	private Date fourDaysLater3pm() {
+		Calendar t = (Calendar) today().clone();
+		t.set(Calendar.AM_PM, Calendar.PM);
+		t.set(Calendar.DATE, t.get(Calendar.DATE) + 4);
+		t.set(Calendar.HOUR, 3);
+
+		return t.getTime();
+	}
+
+	public ScheduleEvent getEvent() {
+		return event;
+	}
+
+	public void setEvent(ScheduleEvent event) {
+		this.event = event;
+	}
+
+	public void addEvent() {
+		if (event.getId() == null)
+			eventModel.addEvent(event);
+		else
+			eventModel.updateEvent(event);
+
+		event = new DefaultScheduleEvent();
+	}
+
+	public void onEventSelect(SelectEvent selectEvent) {
+		event = (ScheduleEvent) selectEvent.getObject();
+	}
+
+	public void onDateSelect(SelectEvent selectEvent) {
+		event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
+	}
+
+	public void onEventMove(ScheduleEntryMoveEvent event) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved",
+				"Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+
+		addMessage(message);
+	}
+
+	public void onEventResize(ScheduleEntryResizeEvent event) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized",
+				"Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+
+		addMessage(message);
+	}
+
+	private void addMessage(FacesMessage message) {
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+	public List<Usuario> getFuncionarios() {
 		return funcionarios;
 	}
 
@@ -272,9 +297,15 @@ public class IndexBean extends AbastractFormBean<HorarioAgendado, HorarioAgendad
 	public void setHorarios(Time horarios) {
 		this.horarios = horarios;
 	}
+
+	public Set<Usuario> getSetFuncionarios() {
+		return setFuncionarios;
+	}
+
+	public void setSetFuncionarios(Set<Usuario> setFuncionarios) {
+		this.setFuncionarios = setFuncionarios;
+	}
 	
 	
-
-
 
 }
--- admin-starter-springboot-master/src/main/java/com/github/adminfaces/starter/repository/UsuarioServicoRepository.java
+++ admin-starter-springboot-master/src/main/java/com/github/adminfaces/starter/repository/UsuarioServicoRepository.java
@@ -1,10 +1,15 @@
 package com.github.adminfaces.starter.repository;
 
-import com.github.adminfaces.starter.model.UsuarioServico;
+
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.github.adminfaces.starter.model.Servico;
+import com.github.adminfaces.starter.model.UsuarioServico;
+
 
 public interface UsuarioServicoRepository extends JpaRepository<UsuarioServico, Integer> {
-
+	List<UsuarioServico> findByServicoOrderByUsuario(Servico servico);
 }

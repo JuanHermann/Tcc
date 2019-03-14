90f2c73a076de486ffebc2b46883c3a8bd3277d5
Ruanoid <35999705+Ruanoid@users.noreply.github.com>
2019-03-14 11:14:56 -0300

    att

--- admin-starter-springboot-master/src/main/java/com/github/adminfaces/starter/model/Servico.java
+++ admin-starter-springboot-master/src/main/java/com/github/adminfaces/starter/model/Servico.java
@@ -7,7 +7,6 @@ import lombok.Setter;
 
 import java.io.Serializable;
 import java.sql.Time;
-import java.util.Objects;
 
 @Entity
 @Getter
@@ -15,7 +14,9 @@ import java.util.Objects;
 @Table(name = "servico")
 public class Servico implements Serializable {
 
-    @Id
+	private static final long serialVersionUID = 1L;
+
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Integer id;
 
@@ -31,29 +32,4 @@ public class Servico implements Serializable {
     @Column(length = 200,nullable = true)
     private String descricao;
 
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Servico other = (Servico) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
 }

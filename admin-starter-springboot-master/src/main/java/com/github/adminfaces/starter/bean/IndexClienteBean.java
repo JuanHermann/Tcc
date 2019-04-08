485214be98b8a293b4a8cc8a35aaa284bac7be16
Ruanoid <35999705+Ruanoid@users.noreply.github.com>
2019-04-08 17:02:22 -0300

    att

--- admin-starter-springboot-master/src/main/java/com/github/adminfaces/starter/bean/IndexClienteBean.java
+++ admin-starter-springboot-master/src/main/java/com/github/adminfaces/starter/bean/IndexClienteBean.java
@@ -4,7 +4,9 @@ import com.github.adminfaces.starter.model.HorarioAgendado;
 import com.github.adminfaces.starter.model.Servico;
 import com.github.adminfaces.starter.model.Usuario;
 import com.github.adminfaces.starter.repository.HorarioAgendadoRepository;
+import com.github.adminfaces.starter.repository.PermissaoRepository;
 import com.github.adminfaces.starter.repository.UsuarioRepository;
+import com.github.adminfaces.starter.repository.UsuarioServicoRepository;
 import com.github.adminfaces.starter.service.UsuarioService;
 
 import lombok.Getter;
@@ -26,12 +28,16 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 @Scope("view")
-public class IndexClienteBean extends AbastractFormBean<Usuario, UsuarioRepository> {
+public class IndexClienteBean extends AbastractFormBean<HorarioAgendado, HorarioAgendadoRepository> {
 
 	@Autowired
 	private UsuarioLogadoBean usuarioLogadoBean;
 	@Autowired
 	private HorarioAgendadoRepository horarioAgendadoRepository;
+	@Autowired
+	private PermissaoRepository permissaoRepository;
+	@Autowired
+	private UsuarioServicoRepository usuarioServicoRepository;
 
 	private MenuModel menuModel;
 
@@ -43,23 +49,95 @@ public class IndexClienteBean extends AbastractFormBean<Usuario, UsuarioReposito
 	public void init() throws InstantiationException, IllegalAccessException {
 		setId(usuarioLogadoBean.getUsuario().getId());
 		menuModel = new DefaultMenuModel();
-		for(HorarioAgendado horario: horarioAgendadoRepository.findByClienteOrderByHoraInicio(usuarioLogadoBean.getUsuario())) {
+		for (HorarioAgendado horario : horarioAgendadoRepository
+				.findByClienteOrderByHoraInicio(usuarioLogadoBean.getUsuario())) {
 
+			DefaultSubMenu submenu = new DefaultSubMenu(horario.getData().toString() + " - "
+					+ horario.getHoraInicio().toString() + " - " + horario.getUsuarioServico().getServico().getNome()); // Cria
+																														// o
+																														// submenu
 
-		DefaultSubMenu submenu = new DefaultSubMenu(horario.getData().toString()+" - "+horario.getHoraInicio().toString() +" - " + horario.getUsuarioServico().getServico().getNome()); // Cria o submenu
+			DefaultMenuItem item = new DefaultMenuItem(); // Cria o menuitem
 
-		DefaultMenuItem item = new DefaultMenuItem(); // Cria o menuitem
+			item.setValue(horario.getHoraTermino() + horario.getUsuarioServico().getUsuario().getNome());
+			item.setStyleClass("btn-info");
+			submenu.addElement(item); // adiciona o menuitem ao submenu
 
-		item.setValue(horario.getHoraTermino() + horario.getUsuarioServico().getUsuario().getNome());
-		item.setStyleClass("btn-info");
-		submenu.addElement(item); // adiciona o menuitem ao submenu
-
-		
-
-		menuModel.addElement(submenu); // adiciona o submenu ao menu
+			menuModel.addElement(submenu); // adiciona o submenu ao menu
 		}
 		super.init();
 	}
+	public boolean mostrarFuncionario() {
+		return permissaoRepository.findByNome("ROLE_FUNCIONARIO").getUsuarios().size() > 1;
 
-	
+	}
+
+	public void salvarAgendamento() {
+			if (mostrarFuncionario()) {
+				for (Servico servico : servicosSelecionados) {
+					getObjeto().setUsuarioServico(
+							usuarioServicoRepository.findByServicoAndUsuarioOrderByUsuario(servico, funcionario));
+				}
+			} else {
+				if (getObjeto().getId() != null) {
+					HorarioAgendado agendado;
+					boolean primeiro = true;
+					LocalTime tempo = LocalTime.of(0, 0, 0);
+					for (Servico servico : servicosSelecionados) {
+
+						if (primeiro) {
+							agendado = getObjeto();
+							agendado.setHoraInicio(getObjeto().getHoraInicio());
+
+							tempo = somarLocalTime(getObjeto().getHoraInicio(), servico.getTempo());
+							agendado.setHoraTermino(tempo);
+							primeiro = false;
+						} else {
+							agendado = new HorarioAgendado();
+							agendado.setCliente(getObjeto().getCliente());
+							agendado.setData(getObjeto().getData());
+							agendado.setHoraInicio(tempo);
+							tempo = somarLocalTime(tempo, servico.getTempo());
+							agendado.setHoraTermino(tempo);
+						}
+						agendado.setUsuarioServico(usuarioServicoRepository.findByServico(servico));
+						getRepository().save(agendado);
+
+					}
+					setObjeto(new HorarioAgendado());
+					servicosSelecionados.clear();
+					atualizarSchedule();
+
+				} else {
+					HorarioAgendado agendado;
+					boolean primeiro = true;
+					LocalTime tempo = LocalTime.of(0, 0, 0);
+					for (Servico servico : servicosSelecionados) {
+						agendado = new HorarioAgendado();
+						agendado.setCliente(getObjeto().getCliente());
+						agendado.setData(getObjeto().getData());
+						if (primeiro) {
+							agendado.setHoraInicio(getObjeto().getHoraInicio());
+
+							tempo = somarLocalTime(getObjeto().getHoraInicio(), servico.getTempo());
+							agendado.setHoraTermino(tempo);
+							primeiro = false;
+						} else {
+							agendado.setHoraInicio(tempo);
+							tempo = somarLocalTime(tempo, servico.getTempo());
+							agendado.setHoraTermino(tempo);
+						}
+						agendado.setUsuarioServico(usuarioServicoRepository.findByServico(servico));
+						getRepository().save(agendado);
+
+					}
+					setObjeto(new HorarioAgendado());
+					servicosSelecionados.clear();
+					atualizarSchedule();
+				}
+			}
+		
+
+	}
+
 }

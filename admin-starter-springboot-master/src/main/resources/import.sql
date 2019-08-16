d524b7153b4213cccd627f2dafe108b043ba8d92
Ruanoid <35999705+Ruanoid@users.noreply.github.com>
2019-08-16 12:58:21 -0300

    att

--- admin-starter-springboot-master/src/main/resources/import.sql
+++ admin-starter-springboot-master/src/main/resources/import.sql
@@ -5,12 +5,12 @@ INSERT INTO servico (nome,tempo,valor,descricao,ativo) VALUES ('Massagem','00:30
 INSERT INTO servico (nome,tempo,valor,descricao,ativo) VALUES ('Sobrancelha','00:15:00',12,'muito bom',true);
 INSERT INTO servico (nome,tempo,valor,descricao,ativo) VALUES ('Microagulhamento','01:00:00',50,'',true);
 INSERT INTO servico (nome,tempo,valor,descricao,ativo) VALUES ('Peeling de Diamante','00:30:00',100,'muito bom',true);
-INSERT INTO usuario (aceito, ativo, email, nome, senha, telefone) VALUES ( false, true, 'julio@adm.com','Julio', 123, '(99) 99123-9119');
-INSERT INTO usuario (aceito, ativo, email, nome, senha, telefone) VALUES ( false, true, 'maria@adm.com','Maria', 123, '(99) 99312-9129');
-INSERT INTO usuario (aceito, ativo, email, nome, senha, telefone) VALUES ( true, true, 'debora@adm.com','Debora', 123, '(99) 99333-6798');
-INSERT INTO usuario (aceito, ativo, email, nome, senha, telefone) VALUES ( true, true, 'roberto@adm.com','Roberto', 123, '(99) 99999-9999');
-INSERT INTO usuario (aceito, ativo, email, nome, senha, telefone) VALUES ( true, true, 'lucas@adm.com','Lucas', 123, '(99) 99999-9999');
-INSERT INTO usuario (aceito, ativo, email, nome, senha, telefone) VALUES ( true, true, 'simone@adm.com','Simone', 123, '(99) 99999-9999');
+INSERT INTO usuario ( ativo, email, nome, senha, telefone) VALUES (  true, 'julio@adm.com','Julio', 123, '(99) 99123-9119');
+INSERT INTO usuario ( ativo, email, nome, senha, telefone) VALUES (  true, 'maria@adm.com','Maria', 123, '(99) 99312-9129');
+INSERT INTO usuario ( ativo, email, nome, senha, telefone) VALUES (  true, 'debora@adm.com','Debora', 123, '(99) 99333-6798');
+INSERT INTO usuario ( ativo, email, nome, senha, telefone) VALUES ( true, 'roberto@adm.com','Roberto', 123, '(99) 99999-9999');
+INSERT INTO usuario ( ativo, email, nome, senha, telefone) VALUES (  true, 'lucas@adm.com','Lucas', 123, '(99) 99999-9999');
+INSERT INTO usuario ( ativo, email, nome, senha, telefone) VALUES (  true, 'simone@adm.com','Simone', 123, '(99) 99999-9999');
 INSERT INTO permissao(id, nome) VALUES (1, 'ROLE_ADMIN');
 INSERT INTO permissao(id, nome) VALUES (2, 'ROLE_FUNCIONARIO');
 INSERT INTO permissao(id, nome) VALUES (3, 'ROLE_ATENDENTE');
